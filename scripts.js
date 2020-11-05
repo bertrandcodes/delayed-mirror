ce400d075ac5975d7c4769442421899f30377261
Bertrand Shao <bertrandshao@gmail.com>
2020-11-04 21:20:53 -0800

    feat: continuous video stream achieved

--- scripts.js
+++ scripts.js
@@ -3,23 +3,31 @@ const video = document.querySelector('.player');
 function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(localMediaStream => {
+            const chunks = []
             const mediaRecorder = new MediaRecorder(localMediaStream)
-            console.log(mediaRecorder.ondataavailable)
-            console.log(mediaRecorder, 'recorder begin')
-            mediaRecorder.start()
-            setTimeout(() => {
-                console.log("stopping");
-                mediaRecorder.stop();
-                console.log(mediaRecorder, 'recorder end')
-                console.log(mediaRecorder.ondataavailable)
-            }, 5000);
 
-            mediaRecorder.ondataavailable = function (e) {
-                console.log(e.data, 'avail')
-                const blob = new Blob([e.data], { type: 'video/ogg' })
+            const record = () => {
+                mediaRecorder.start()
+                setTimeout(() => {
+                    // console.log("stopping");
+                    mediaRecorder.stop()
+                    // console.log(mediaRecorder, 'recorder end')
+                    // console.log(mediaRecorder.ondataavailable)
+                    getVideo()
+                }, 5000);
+            }
+
+            record()
+
+            mediaRecorder.onstop = e => {
+                const blob = new Blob(chunks, { type: 'video/ogg' })
                 var videoURL = URL.createObjectURL(blob);
                 video.src = videoURL;
                 video.play();
+            }
+
+            mediaRecorder.ondataavailable = e => {
+                chunks.push(e.data);
             }
 
             // set video with current stream
