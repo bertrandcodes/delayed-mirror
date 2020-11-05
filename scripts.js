a26427f54e2248e79b589689b8bbd5550f0ec28b
Bertrand Shao <bertrandshao@gmail.com>
2020-11-04 20:16:16 -0800

    feat: figure out record and playback

--- scripts.js
+++ scripts.js
@@ -3,9 +3,28 @@ const video = document.querySelector('.player');
 function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(localMediaStream => {
-            console.log(localMediaStream)
-            video.srcObject = localMediaStream;
-            video.play();
+            const mediaRecorder = new MediaRecorder(localMediaStream)
+            console.log(mediaRecorder.ondataavailable)
+            console.log(mediaRecorder, 'recorder begin')
+            mediaRecorder.start()
+            setTimeout(() => {
+                console.log("stopping");
+                mediaRecorder.stop();
+                console.log(mediaRecorder, 'recorder end')
+                console.log(mediaRecorder.ondataavailable)
+            }, 5000);
+
+            mediaRecorder.ondataavailable = function (e) {
+                console.log(e.data, 'avail')
+                const blob = new Blob([e.data], { type: 'video/ogg' })
+                var videoURL = URL.createObjectURL(blob);
+                video.src = videoURL;
+                video.play();
+            }
+
+            // set video with current stream
+            // video.srcObject = localMediaStream;
+            // video.play();
         })
         .catch(err => {
             console.error('No webcam!', err)
@@ -14,6 +33,5 @@ function getVideo() {
 
 getVideo();
 
-// const mediaRecorder = new MediaRecorder()
 
 // console.log(mediaRecorder)
